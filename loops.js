00767cd0bc7b5c6aa90500929999a53196fae540
Anthony Lam <al3101@nyu.edu>
2018-04-26 01:51:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,10 +19,10 @@ function whileLoop(num){
   return 'done'
 }
 
-
-function doWhileLoop(array) {
-  function maybeTrue() {
+function maybeTrue() {
   return Math.random() >= 0.5
 }
  
+function doWhileLoop(array) {
+  
 }
