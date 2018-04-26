d0e54ddd256e4f465c8c5894563e1f40ae7846f4
Anthony Lam <al3101@nyu.edu>
2018-04-26 01:34:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,13 @@
 Function forLoop() {
-  
-}
-for (var i = 0; i < 25; i++) {
-  if (i == 0) {
-    console.log("I am 1 strange loop.")
-  } else {
-    console.log("I am ${i} strange loops.")
+  for (var i = 0; i < 25; i++) {
+    if (i == 0) {
+      console.log("I am 1 strange loop.")
+      
+    } else {
+      console.log("I am ${i} strange loops.")
+      
+    }
+    
   }
 }
 
